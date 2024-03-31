d1c2b20690e28a7ed7fab7bb17b12af3f38df00f
Lyvia Huang <126723293+kesesek@users.noreply.github.com>
2024-04-01 12:50:06 +1300

    add pics

--- 732Demo/public/dummyData.js
+++ 732Demo/public/dummyData.js
@@ -1,6 +1,16 @@
 export const albums = [
-    {name: '', imgSrc: ''},
-    {name: '', imgSrc: ''},
-    {name: '', imgSrc: ''},
-    {name: '', imgSrc: ''},
+    {name: 'K.K. Animal City', imgSrc: './pics/albums/Animal_City_NH_Texture.png'},
+    {name: 'HERE A PEOPLE SOWS', imgSrc: './pics/albums/IMG_1288.JPG'},
+    {name: 'Deep Purple', imgSrc: './pics/albums/IMG_1289.JPG'},
+    {name: 'Comedown Machine', imgSrc: './pics/albums/IMG_1290.JPG'},
+    {name: '(What\'s The Story)Morning Glory?', imgSrc: './pics/albums/IMG_1291.JPG'},
+    {name: 'In Utero', imgSrc: './pics/albums/IMG_1292.JPG'},
+];
+
+export const artists = [
+    {name: 'ROBO_HEAD', imgSrc: './pics/artists/IMG_1111.PNG'},
+    {name: 'Arctic Monkeys', imgSrc: './pics/artists/IMG_1294.jpg'},
+    {name: 'Emperor', imgSrc: './pics/artists/IMG_1296.JPG'},
+    {name: 'Meowsician', imgSrc: './pics/artists/IMG_7383.JPG'},
+    {name: 'K.K.Slider', imgSrc: './pics/artists/K.K._Slider_NH.webp'},
 ];
--- 732Demo/src/components/HomePage.vue
+++ 732Demo/src/components/HomePage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-import styles from '../assets/scss/homepage.module.scss'
+import styles from '../assets/scss/homepage.module.scss';
+import { albums, artists } from '../../public/dummyData';
 
 defineProps({
   username: {
@@ -19,11 +20,17 @@ defineProps({
         <div :class="styles.popular_albums">
             <h2>Popular albums</h2>
             <RouterLink to="#">Show all</RouterLink>
+
+            <!--use v-for and v-bind to iterate dummy data and show all items.  
+                `v-bind` can be simplified as `:` to tell Vue that this is
+                 a property that needs to be bound to a JavaScript expression, 
+                 rather than a normal static property.
+            -->
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li v-for="(album, index) in albums" v-bind:key="index">
+                    <img :src="album.imgSrc">
+                    {{ album.name }}
+                </li>
             </ul>
         </div>
 
@@ -31,13 +38,10 @@ defineProps({
             <h2>Popular artists</h2>
             <RouterLink to="#">Show all</RouterLink>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li v-for="(artist, index) in artists" :key="index">
+                    <img :src="artist.imgSrc">
+                    {{ artist.name }}
+                </li>
             </ul>
         </div>
     </div>
