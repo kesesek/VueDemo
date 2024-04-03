f05ca27c7c283531313794853c6f30eae10721e0
Lyvia Huang <126723293+kesesek@users.noreply.github.com>
2024-04-04 12:03:12 +1300

    edit

--- 732Demo/src/views/OtherPages.vue
+++ 732Demo/src/views/OtherPages.vue
@@ -3,7 +3,7 @@ import { useRoute } from 'vue-router'
 import { computed } from 'vue'
 const route = useRoute()
 const lastSegment = computed(() => {
-  const segments = route.path.split('/').filter(Boolean) // 分割路径并移除空字符串
+  const segments = route.path.split('/').filter(Boolean)
   return segments.length > 0 ? segments[segments.length - 1] : ''
 })
 </script>
